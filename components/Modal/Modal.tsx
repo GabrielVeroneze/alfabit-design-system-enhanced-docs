613185092b723e97b7aa5de1d89f2607b1002b7a
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-11-20 16:52:49 -0300

    ✨ feat: cria componente Modal com animações e suporte a título e fechamento
    Adiciona o componente Modal utilizando Headless UI, incluindo transições de entrada e saída, painel estruturado, botão de fechamento e suporte para título e conteúdo.

---
+++ components/Modal/Modal.tsx
@@ -0,0 +1,60 @@
+import {
+    Dialog,
+    DialogPanel,
+    DialogTitle,
+    Transition,
+    TransitionChild,
+} from '@headlessui/react'
+import { XMarkIcon } from '@heroicons/react/24/outline'
+
+export interface ModalProps extends React.HtmlHTMLAttributes<HTMLElement> {
+    children: string
+    title: string
+    isOpen: boolean
+    onClose: () => void
+}
+
+const Modal = ({ children, title, isOpen, onClose }: ModalProps) => {
+    return (
+        <Transition appear show={isOpen}>
+            <Dialog className="" onClose={onClose}>
+                <TransitionChild
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className="" />
+                </TransitionChild>
+                <div className="">
+                    <div className="">
+                        <TransitionChild
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 scale-95"
+                            enterTo="opacity-100 scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 scale-100"
+                            leaveTo="opacity-0 scale-95"
+                        >
+                            <DialogPanel className="">
+                                <div className="">
+                                    <DialogTitle as="h3" className="">
+                                        {title}
+                                    </DialogTitle>
+                                    <button onClick={onClose}>
+                                        <XMarkIcon className="" />
+                                    </button>
+                                </div>
+                                {children}
+                            </DialogPanel>
+                        </TransitionChild>
+                    </div>
+                </div>
+            </Dialog>
+        </Transition>
+    )
+}
+
+export default Modal
