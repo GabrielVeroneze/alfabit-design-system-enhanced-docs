46466fbd7f8d57dc1119c20dbd2c76dc3442fb1a
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-11-21 16:24:00 -0300

    ✨ feat: adiciona stories iniciais do componente Dropdown no Storybook
    Cria arquivo de stories para o componente Dropdown, incluindo configuração base do Storybook, decorators para espaçamento e duas variantes: Default e
    Common. Permite visualizar o componente com listas vazias e listas populares para testes e documentação.

---
+++ components/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,36 @@
+import { Meta, StoryObj } from '@storybook/nextjs-vite'
+import Dropdown, { DropdownProps } from './Dropdown'
+
+const meta = {
+    title: 'Molecules/Dropdown',
+    component: Dropdown,
+    decorators: [
+        (Story) => (
+            <div style={{ padding: '3rem 0 15rem' }}>
+                <Story />
+            </div>
+        ),
+    ],
+} satisfies Meta<DropdownProps>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+    args: {
+        list: [],
+    },
+}
+
+export const Common: Story = {
+    args: {
+        list: [
+            'Durward Reynolds',
+            'Kenton Towne',
+            'Therese Wunsch',
+            'Benedict Kessler',
+            'Katelyn Rohan',
+        ],
+    },
+}
